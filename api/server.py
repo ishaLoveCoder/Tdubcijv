c96908a92e22f35a848e44dc3bd77ae4162f33ee
ishaLoveCoder <isha1470@outlook.com>
2026-03-18 10:13:13 +0530

    Update server.py

--- api/server.py
+++ api/server.py
@@ -4,15 +4,27 @@ from database import movies
 app = FastAPI()
 
 
-@app.get("/movies")
-async def get_movies():
+@app.get("/")
+async def home():
 
     data = []
 
     async for m in movies.find():
 
         m["_id"] = str(m["_id"])
+        data.append(m)
 
+    return data
+
+
+@app.get("/search")
+async def search(q: str):
+
+    data = []
+
+    async for m in movies.find({"title": {"$regex": q, "$options": "i"}}):
+
+        m["_id"] = str(m["_id"])
         data.append(m)
 
     return data

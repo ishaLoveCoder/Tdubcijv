932012400e904a7d4ab3677242b18c1814c61fd9
ishaLoveCoder <isha1470@outlook.com>
2026-03-18 10:14:22 +0530

    Update imdb_fetch.py

--- imdb_fetch.py
+++ imdb_fetch.py
@@ -1,25 +1,15 @@
 import requests
 from config import TMDB_KEY
 
-async def get_movie(imdb):
+async def get_poster_from_tmdb(imdb_id):
 
-    url = f"https://api.themoviedb.org/3/find/{imdb}?api_key={TMDB_KEY}&external_source=imdb_id"
+    url = f"https://api.themoviedb.org/3/find/{imdb_id}?api_key={TMDB_KEY}&external_source=imdb_id"
 
     r = requests.get(url).json()
 
     if not r["movie_results"]:
-        return {}
+        return ""
 
     m = r["movie_results"][0]
 
-    return {
-
-        "title": m["title"],
-
-        "description": m["overview"],
-
-        "poster": "https://image.tmdb.org/t/p/w500" + m["poster_path"],
-
-        "year": m["release_date"][:4]
-
-    }
+    return "https://image.tmdb.org/t/p/w500" + m["poster_path"]
